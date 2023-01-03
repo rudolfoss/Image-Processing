4f654b896ae561eaaf9ea9a1bbf65210e99d61d0
rudolfoss <sorktjsdldu1@naver.com>
2023-01-03 10:13:24 +0900

    2023/1/3

---
+++ Boundary and Filling/Boundary and Filling.cpp
@@ -0,0 +1,52 @@
+﻿//201821054 미디어학과 하태선 
+
+#include <iostream>
+#include <vector>
+#include <ctime>
+#include<stdlib.h>
+#include "opencv2/core.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+
+using namespace std;
+using namespace cv;
+
+Mat input;
+Mat output;
+Mat boundary;
+Mat temp;
+
+void mouseAction(int event, int x, int y, int flags, void* param) {
+    if (event == EVENT_LBUTTONDOWN) {
+        Mat se = getStructuringElement(MORPH_CROSS, Size(3, 3));
+        Mat rf = input;
+        Mat rb;
+        bitwise_not(boundary, rb);
+        rf = rf.zeros(rf.rows, rf.cols, CV_8UC1);
+        rf.at<uchar>(y, x) = 255;
+
+        imshow("output", rf);
+        while (1)
+        {
+            dilate(rf, rf, se);
+            bitwise_and(rf, rb, rf);
+            waitKey(1);
+            imshow("output", rf);
+        }
+    }
+}
+
+int main() {
+    Mat se = getStructuringElement(MORPH_RECT, Size(3, 3));
+    input = imread("C:\\Users\\user\\Desktop\\Profile.png", 0);
+    erode(input, temp, se);
+    boundary = input - temp;
+
+    namedWindow("output");
+    setMouseCallback("output", mouseAction);
+    imshow("input", input);
+    imshow("output", boundary);
+
+    waitKey();
+    return 0;
+}
---
+++ Median Filtering/Median Filtering.cpp
@@ -0,0 +1,40 @@
+﻿//201821054 미디어학과 하태선 1차과제
+
+#include<iostream>
+#include<array>
+#include<vector>
+#include <opencv2/highgui.hpp>
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+
+using namespace cv;
+using namespace std;
+
+
+void myMedianFilter(InputArray input, OutputArray output, int windowSize) {
+    vector<uchar> vec;
+    const Mat& img = input.getMat();
+    output.create(img.size(), img.type());
+    Mat dst = output.getMat();
+    int window = (windowSize-1)/2;
+    for (auto y = 0; y < img.rows; y++) 
+        for (auto x = 0; x < img.cols; x++) {
+            vec.clear();
+            for (auto t = -window; t <= window; t++)
+                for (auto s = -window; s <= window; s++) {
+                    vec.push_back(img.at<uchar>(min(img.rows - 1, max(0, y + t)), min(img.cols - 1, max(0, x + s))));
+        }
+           sort(vec.begin(),vec.end());
+           dst.at<uchar>(y, x) = vec[vec.size()/2];
+    }
+}
+int main()
+{
+    Mat image = imread("C:\\Users\\user\\Desktop\\MedianFilterInput.png",0);    
+    Mat out;
+    myMedianFilter(image, out, 5);
+    imshow("image", image);
+    imshow("median", out);
+    waitKey();
+    return 0;
+}
